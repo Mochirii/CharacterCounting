ea544e7054b209a6ff6b5d7eabeec74d5dfa7c5c
Mel Smith <melpcsmith@gmail.com>
2017-10-04 20:42:42 +0000

    first commit
    process.exit(1)
    process.exit(1);
    .exit

---
+++ countletters.js
@@ -0,0 +1,12 @@
+
+function countLetters(charSpace) {
+  var charMap = {}
+  for (var i = 0; i < charSpace.length; i++){
+    var mychar = charSpace[i].toLowerCase()
+    if (mychar !== " ") {
+      charMap[mychar] = charMap[mychar] + 1 || 1
+    }
+  }
+return JSON.stringify(charMap,null,4)
+};
+console.log(countLetters("Lighthouse in the house"));
